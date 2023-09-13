a916fb162e027186b116e24f18e8102d3cf58862
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2023-09-13 14:48:25 +0200

    Waypoint system toegevoegd

--- TDgame/Assets/Scripts/WaypointFollower.cs
+++ TDgame/Assets/Scripts/WaypointFollower.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class WaypointFollower : MonoBehaviour
 {
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private int nextWaypointIndex = 1;
+    [SerializeField] private float reachedWaypointClearance = 0.25f;
 
+    private void Start()
+    {
+        transform.position = waypoints[0].position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[nextWaypointIndex].position, Time.deltaTime * speed);
+        if (Vector3.Distance(transform.position, waypoints[nextWaypointIndex].position) <= reachedWaypointClearance)
+        {
+            nextWaypointIndex++;
+        }
+        if (nextWaypointIndex >= waypoints.Length)
+        {
+            nextWaypointIndex = 0;
+        }
+    }
 }
